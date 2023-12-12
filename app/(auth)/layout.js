12d25b360164d2dc679af080fbf7a242022c8c0e
esih84 <esmaeilh.haji84@gmail.com>
2023-12-13 03:12:16 +0330

    clerk setup

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+const layout = ({children}) => {
+    return (
+        <div className="flex items-center justify-center h-full ">
+            {children}
+        </div>
+    );
+}
+
+export default layout;
--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className=" p-4">
-      <p>Admin Dashboard</p>
+      <UserButton afterSignOutUrl="/"/>
+      
     </div>
   )
 }
